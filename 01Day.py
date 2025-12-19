2e579ce0a18ecbe3ddeef8877ffd8fa4b45d0258
dbarkins0 <dbarkins@calpoly.edu>
2025-12-19 09:00:40 -0800

    Add files via upload

---
+++ 01Day.py
@@ -0,0 +1,23 @@
+# Intro 
+# Day 1 of 30 
+
+print(3 + 2) # add 
+print(3 - 2) # sub
+print(3 * 2) # multiple
+print(3 / 2) # divide 
+print(3 ** 2) # exponential
+print(3 // 2) # floor division
+print(3 % 2) # modulus
+
+# Data types 
+
+print(type(10)) # int
+print(type(3.14)) # float
+print(type(1 + 3j)) # complex
+print(type('darren')) # string
+print(type([1, 2, 3])) # list
+print(type({'name': 'darren'})) # dictionary
+print(type({9.8, 3.14, 2.7}))    # set
+print(type((9.8, 3.14, 2.7)))    # tuple
+print(type(3 == 3))              # bool
+print(type(3 >= 3))              # bool
